a719a75abc12e9c6b1133b20e34af8763d45ed9a
Wilfrido Bonett <wbonett10@gmail.com>
2020-05-05 00:49:32 -0500

    work in progress

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,6 @@
 class Person < ApplicationRecord
     validates :name, presence: true
+
+    has_many :movie_person
+    has_many :movie, through: :movie_person
 end
--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,6 +1,5 @@
 class MovieSerializer < ActiveModel::Serializer
   attributes :id, :title, :year, :synopsis
 
-  has_many :movie_person
-  # has_many :people
+  has_many :movie_person  
 end
